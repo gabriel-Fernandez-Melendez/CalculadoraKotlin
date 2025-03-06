0bd309a78be8672b3f5333f313430caa2c469ce1
unknown <yic00732@educastur.es>
2025-03-06 01:56:30 +0100

    cambios en como se recibe el valor de los numeros

--- app/src/main/java/com/example/micalculadora/MVVM/viewmodel/ViewModelCalculadora.kt
+++ app/src/main/java/com/example/micalculadora/MVVM/viewmodel/ViewModelCalculadora.kt
@@ -4,10 +4,17 @@ import android.util.Log
 import androidx.compose.material3.ButtonColors
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
 class ViewModelCalculadora : ViewModel(){
 
+    private  val _operacion = MutableStateFlow("")
+    val cambiardatos =_operacion.asStateFlow()
+
+    private  val _resultado = MutableStateFlow("")
+    val resultado =_resultado.asStateFlow()
     //este metodo dara color a los botones en funcion del texto que recibe como parametro
     fun ColorDeLosBotones(boton :String): Color { //recordar que el tipo de variable que se retorna en kotlin va delante
         if (boton.equals(" ")||boton.equals("=")){
@@ -17,6 +24,21 @@ class ViewModelCalculadora : ViewModel(){
     }
 
     fun Boton_Dato(texto : String){
-        Log.i("se preciono el boton",texto)
+        Log.i("se preciono el : ",texto) //para testear que se conecto bien la clase
+        //hay que meter esta condicional del que el objeto no sea nulo para poder borrar y concatenar numeros en la cadena
+        _operacion.value?.let {
+            if (texto.equals("C")){
+                _operacion.value =""
+                return
+            }
+            if (texto.equals("=")){
+                _operacion.value =_resultado.value
+                return
+            }
+
+            _operacion.value=it+texto
+            Log.i("los numeros son",_operacion.value.toString())
+        }
+
     }
 }
